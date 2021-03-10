f61a64b994413dec27b1a84ba88d5f5fd889d445
darsvador <Lx3JQkmzRS@protonmail.com>
2021-03-10 23:24:10 +0800

    add murmur3 crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use fasthash::murmur3::hash32_with_seed as hash_with_seed;
 use openssl::rand::rand_bytes;
+use std::io::Cursor;
+use murmur3::murmur3_32;
 use std::num::Wrapping;
 pub trait Murmur3 {
     fn murmur_hash<T: AsRef<[u8]>>(self, bytes: T) -> u32;
@@ -7,19 +9,18 @@ pub trait Murmur3 {
 
 impl Murmur3 for u32 {
     fn murmur_hash<T: AsRef<[u8]>>(self, bytes: T) -> u32 {
-        let len = Wrapping(bytes.as_ref().len() as u32);
+        let ptr = bytes.as_ref().as_ptr();
+        let len = bytes.as_ref().len();
         let mut h = Wrapping(self);
         let c1 = Wrapping(0xcc9e2d51u32);
         let c2 = Wrapping(0x1b873593u32);
 
         unsafe {
-            let bytes = bytes.as_ref();
             let (block, left): (&[u32], &[u8]) = {
-                let rest_len = bytes.len();
-                let (us_len, ts_len) = (rest_len / 4, rest_len & 3);
+                let (us_len, ts_len) = (len / 4, len & 3);
                 (
-                    std::slice::from_raw_parts(bytes.as_ptr() as *const u32, us_len),
-                    std::slice::from_raw_parts(bytes.as_ptr().add(bytes.len() - ts_len), ts_len),
+                    std::slice::from_raw_parts(ptr as *const u32, us_len),
+                    std::slice::from_raw_parts(ptr.add(len - ts_len), ts_len),
                 )
             };
             for &k in block.iter() {
@@ -59,7 +60,7 @@ impl Murmur3 for u32 {
                 0 => {}
                 _ => core::hint::unreachable_unchecked(),
             }
-            h ^= len;
+            h ^= Wrapping(len as u32);
             h ^= h >> 16;
             h *= Wrapping(0x85ebca6b);
             h ^= h >> 13;
@@ -74,9 +75,10 @@ fn main() {
     use std::time::Instant;
     let mut buf = [0; 85];
     rand_bytes(&mut buf).unwrap();
-    let run_times = 10000000;
+    let run_times = 100000000;
     let t0: u128;
     let t1: u128;
+    let t2: u128;
     let now = Instant::now();
     let mut h0 = 0u32;
     for _ in 0..run_times {
@@ -89,8 +91,14 @@ fn main() {
         h1 = hash_with_seed(&buf, h1);
     }
     t1 = now.elapsed().as_millis();
-    if h1 == h0 {
-        println!("ffi time cost:{}ms, native rust time cost:{}ms", t1, t0);
+    let now = Instant::now();
+    let mut h2 = 0u32;
+    for _ in 0..run_times {
+        h2 = murmur3_32(&mut Cursor::new(buf), h2).unwrap();
+    }
+    t2 = now.elapsed().as_millis();
+    if h1 == h0 && h0==h2{
+        println!("ffi time cost:{}ms, native rust time cost:{}ms, murmur3 crate time cost:{}ms", t1, t0, t2);
         println!(
             "ffi is faster than native rust:{}%",
             ((t0 as f64 - t1 as f64) / t1 as f64) * 100.0
